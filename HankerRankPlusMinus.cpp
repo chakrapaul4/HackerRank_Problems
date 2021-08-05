87ed2aa18641e27cbf97aa22ca9fe9668264d58a
chakrapaul4 <62139138+chakrapaul4@users.noreply.github.com>
2021-08-05 12:47:20 +0530

    Add files via upload

---
+++ HankerRankPlusMinus.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	int arr[n];
+    float positive = 0.0;
+     float negative = 0.0 ;
+    float zero = 0.0;
+	for(int i=0 ;i<n;i++){
+		cin>>arr[i];
+		if(arr[i]>0)
+		{
+			positive++;
+		}
+		else if(arr[i]<0)
+		{
+			negative++;
+		}
+		else 
+		{
+			zero++;
+		}
+	}
+	// cout<<positive<<endl<<negative<<endl<<zero<<endl;
+	double result1,result2,result3;
+	result1 = (positive/n);
+	result2 = (negative/n);
+	result3 = (zero/n);
+	 cout<<setprecision(6)<<result1<<endl<<setprecision(6)<<result2<<endl<<setprecision(6)<<result3<<endl;
+	 return 0; 
+
+
+
+float j = 3 / 6;
+cout<<j;
+}
