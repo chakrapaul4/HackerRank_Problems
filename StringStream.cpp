7c108f0321124f0a62cd1e5fce172cf3e7309f67
chakrapaul4 <62139138+chakrapaul4@users.noreply.github.com>
2021-08-05 12:45:26 +0530

    Add files via upload

---
+++ StringStream.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   string s;
+   cin>>s;
+   for(int i=0;i<s.size();i++)
+   {
+      if(s[i]==',')
+    {
+      s[i]=' ';
+      s[i]='\n';
+    }
+    cout<<s[i];
+   }
+   return 0;
+}
